5e9ed826c3480fe06341600dee8996f289e42f3f
r0302772 <dennis.nijs@hotmail.com>
2022-11-22 11:43:08 +0100

    second commit

---
+++ Music.db/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Music.db.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
